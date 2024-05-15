a4ed545e95ee52cbe1cb2c4c0e2dc2e5d931c618
Boris Volarič <boris@kalvarija.net>
2024-05-15 20:03:54 +0200

    Prvi commit

---
+++ AlkoholnaPijaca.java
@@ -0,0 +1,22 @@
+/**
+ * AlkoholnaPijaca.java
+ * 
+ * Opis:
+ * Vmesnik za alkoholne pijače
+ *
+ */
+
+/**
+ * Javni vmesnik, ki ga morajo implementirati vse steklenice, kozarci in sodi z alkoholnimi pijačami
+ *
+ * @author Boris Volarič
+ * @version Primer 19a (vmesniki)
+ */
+public interface AlkoholnaPijaca {
+	
+	/** Javna abstraktna metoda, ki vrne stopnjo alkohola
+	 * @return Stopnja alkohola
+	 */
+	 public double getStopnjaAlkohola();
+	 
+}
